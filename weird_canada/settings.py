a1f5c4d3b607fb42f704f82d509a2bc9dad0c93e
pattmayne <pattmayne@gmail.com>
2015-06-10 15:39:22 -0400

    fixed settings.py

--- weird_canada/settings.py
+++ weird_canada/settings.py
@@ -93,6 +93,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    "/home/pattmayne/webapps/limbs_008/weird_canada/weird_canada/static/",
+    "/home/pattmayne/webapps/limbs_008/weird_canada/static/",
 
 )
