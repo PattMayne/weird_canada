bfa88f2488c6519ef35f398f3f1a758a9a4fd0cf
pattmayne <pattmayne@gmail.com>
2015-06-10 12:03:25 -0400

    edited models to fix error

--- weird_canada/settings.py
+++ weird_canada/settings.py
@@ -90,3 +90,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    "/home/pattmayne/webapps/limbs_008/weird_canada/weird_canada/static/",
+
+)
