a7a4eeb43f9c9af849a680748daa621fdecbfd36
pattmayne <pattmayne@gmail.com>
2015-06-11 10:54:54 -0400

    Adding new artist forms and pages

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,38 @@ from indie_db.forms import AddArtistForm, AddWorkForm
 # Create your views here.
 
 
-#These three defs are for LIST pages, with ALL the albums, producers, or artists
+#TEST
 def test(request):
     author_form = AddAuthorForm
     entry_form = BlogEntryForm
     artist_form = AddArtistForm
     work_form = AddWorkForm
     return render(request, 'blog/temp.html', {'author_form': author_form, 'entry_form': entry_form, 'work_form': work_form, 'artist_form': artist_form})
+
+
+# Add Info to INDIE_DB (maybe later move to the indie_db views.py file)
+
+def write_new_artist(request):
+    artist_form = AddArtistForm
+    return render(request, 'blog/temp.html', {'artist_form': artist_form})
+
+
+def save_new_artist(request):
+    if request.method == 'POST':
+        form = AddArtistForm(request.POST)
+        artist = None
+        if form.is_valid():
+            artist = form.save()
+            return render(request, 'blog/view_artist.html', {'artist': artist})
+        else:
+            error_message = 'The form was not valid. The data was not saved.'
+            return render(request, 'blog/error.html', {'error_message': error_message})
+
+
+# View raw data from indie_db
+
+def view_artist(request):
+    if request.method == 'POST':
+        if artist in request.POST:
+            artist_id = request.POST.get('artist')
+            return render(request, 'blog/view_artist.html', {'artist': artist})
--- weird_canada/urls.py
+++ weird_canada/urls.py
@@ -10,4 +10,10 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^test/$', 'blog.views.test', name="test_page"),
+    # add stuff to the database
+    url(r'^wc_admin/write_new_artist', 'blog.views.write_new_artist', name="write_new_artist_view"),
+    url(r'^wc_admin/save_new_artist', 'blog.views.save_new_artist', name="save_new_artist_view"),
+    # view raw database info
+    url(r'^indie_db/view_artist', 'blog.views.view_artist', name="view_artist_view"),
+    url(r'^indie_db/view_work', 'blog.views.view_work', name="view_work_view"),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
