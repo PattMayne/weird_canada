e11f4a3636c018ffea337d4ffbdf33d082b6b800
pattmayne <pattmayne@gmail.com>
2015-06-14 14:13:09 -0400

    Fixing up the wc_admin

--- weird_canada/urls.py
+++ weird_canada/urls.py
@@ -10,14 +10,14 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^test/$', 'blog.views.test', name="test_page"),
-    url(r'^wc_admin/', 'blog.views.wc_admin_hub', name="wc_admin_hub_view"),
+    #url(r'^wc_admin/', 'blog.views.wc_admin_hub', name="wc_admin_hub_view"),
 
     # add stuff to the database
     url(r'^wc_admin/write_new_artist/', 'blog.views.write_new_artist', name="write_new_artist_view"),
     url(r'^wc_admin/write_new_work/', 'blog.views.write_new_work', name="write_new_work_view"),
     url(r'^wc_admin/save_new_artist/', 'blog.views.save_new_artist', name="save_new_artist_view"),
     url(r'^wc_admin/save_new_work/', 'blog.views.save_new_work', name="save_new_work_view"),
-    url(r'^wc_admin/write_review/', 'blog.views.write_new_review_article', name="write_new_review_article"),
+    url(r'^wc_admin/write_review/', 'blog.views.write_new_review_article', name="write_new_review_article_view"),
     url(r'^wc_admin/save_review/', 'blog.views.save_new_review_article', name="save_new_review_article_view"),
 
     # view raw database info
