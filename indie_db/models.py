21a304f66d4944a9d62bd59718cce0869cb16111
pattmayne <pattmayne@gmail.com>
2015-06-15 14:08:20 -0400

    added HELP modal

--- indie_db/models.py
+++ indie_db/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from blog.models import Author
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -43,7 +43,7 @@ class Artist(models.Model):
     deathdate = models.DateField(null=True, blank=True)
     group = models.BooleanField(default=False)
     members = models.ManyToManyField('self')
-    author = models.ForeignKey(blog.Author, null=True, blank=True)
+    author = models.ForeignKey(User, null=True, blank=True)
     website = models.ForeignKey(URL, null=True, blank=True)
 
     class Meta:
@@ -63,7 +63,7 @@ class ProductionCompany(models.Model):
     name = models.CharField(max_length=200)
     website = models.ForeignKey(URL)
     location = models.CharField(max_length=200)
-    author = models.ForeignKey(blog.Author, null=True, blank=True)
+    author = models.ForeignKey(User, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -86,7 +86,7 @@ class Work(models.Model):
     website = models.ForeignKey(URL, null=True, blank=True)
     styles = models.ManyToManyField(Style, null=True, blank=True)
     contributors = models.ManyToManyField(Contributor, null=True, blank=True)
-    author = models.ForeignKey(blog.Author, null=True, blank=True)
+    author = models.ForeignKey(User, null=True, blank=True)
 
     class Meta:
         ordering = ['-created']
