5b66061541b76b9b84278670992869740cfa907c
pattmayne <pattmayne@gmail.com>
2015-06-17 10:24:59 -0400

    More stuff for backend saving articles etc.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from blog.models import Article, Tag, Author
+from blog.models import Article, Tag, Author, ArticleCategory
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(ArticleCategory)
--- indie_db/admin.py
+++ indie_db/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from indie_db.models import URL, Artist, Work, Contributor, ProductionCompany
+from indie_db.models import URL, Artist, Work, Contributor, ProductionCompany, WorkCategory
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Work)
+admin.site.register(WorkCategory)
 admin.site.register(URL)
 admin.site.register(Contributor)
 admin.site.register(ProductionCompany)
