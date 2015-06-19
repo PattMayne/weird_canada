7e3cc349e9279eb8c2f1c5cc66fe323a8610fb74
pattmayne <pattmayne@gmail.com>
2015-06-18 23:39:32 -0400

    Adding image file stuff

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from blog.models import Article, Tag, Author, ArticleCategory
+from blog.models import Article, Tag, Author, ArticleCategory, ArticleImage
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(ArticleCategory)
+admin.site.register(ArticleImage)
