c201db2bd02dc7069cda714e0cdac9719ebd5aa8
pattmayne <pattmayne@gmail.com>
2015-06-19 12:16:29 -0400

    markdown stuff

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from blog.models import Article, Tag, Author, ArticleCategory, ArticleImage
 from django_markdown.admin import MarkdownModelAdmin
+from django_markdown.widgets import AdminMarkdownWidget
+from django.db.models import TextField
 
 # Register your models here.
+
+
+class EntryAdmin(MarkdownModelAdmin):
+    formfield_overrides = {TextField: {'widget': AdminMarkdownWidget}}
+
 
 #admin.site.register(Article)
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(ArticleCategory)
 admin.site.register(ArticleImage)
-admin.site.register(Article)
+admin.site.register(Article, EntryAdmin)
