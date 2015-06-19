ae905a1b058f83ea2ccb1ecde29154c80af6c83d
pattmayne <pattmayne@gmail.com>
2015-06-19 12:51:01 -0400

    markdown stuff

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from blog.models import Article, Tag, Author, ArticleCategory, ArticleImage
-from django_markdown.admin import MarkdownModelAdmin
-from django_markdown.widgets import AdminMarkdownWidget
-from django.db.models import TextField
+#from django_markdown.admin import MarkdownModelAdmin
+#from django_markdown.widgets import AdminMarkdownWidget
+#from django.db.models import TextField
 
 # Register your models here.
 
@@ -12,4 +12,3 @@ admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(ArticleCategory)
 admin.site.register(ArticleImage)
-admin.site.register(Article)
