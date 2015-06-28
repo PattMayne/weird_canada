32d9a3ff7d999828e1f3a0c00f29125ccd5483c4
pattmayne <pattmayne@gmail.com>
2015-06-28 00:10:05 -0400

    Fixin the front page

--- indie_db/admin.py
+++ indie_db/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from indie_db.models import URL, Artist, Work, Contributor, ProductionCompany, WorkCategory
+from indie_db.models import URL, Artist, Work, Contributor, ProductionCompany, WorkCategory, WorkRelativeEpoch
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Work)
 admin.site.register(WorkCategory)
+admin.site.register(WorkRelativeEpoch)
 admin.site.register(URL)
 admin.site.register(Contributor)
 admin.site.register(ProductionCompany)
