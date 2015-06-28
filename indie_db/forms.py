8abd38a6f3ecb118c93af89020483d5036d1023a
pattmayne <pattmayne@gmail.com>
2015-06-28 00:38:10 -0400

    Fixin the front page

--- indie_db/forms.py
+++ indie_db/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.forms import ModelForm, TextInput, Select, Textarea, FileInput, NumberInput, CheckboxInput, DateField, DateInput
-from indie_db.models import URL, Artist, Work, Contributor, ProductionCompany, WorkRelativeEpoch, Style
+from indie_db.models import URL, Artist, Work, Contributor, ProductionCompany, Style
 from blog.models import Article, Tag, Author
 
 
