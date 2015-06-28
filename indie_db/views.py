8dfbaf7d45acf7f2465664cd0a69f2d20890f460
pattmayne <pattmayne@gmail.com>
2015-06-28 15:02:04 -0400

    Front site - categories

--- indie_db/views.py
+++ indie_db/views.py
@@ -90,6 +90,46 @@ def index(request):
 
 def article(request):
     if request.method == 'GET' and 'id' in request.GET:
+        categories = ArticleCategory.objects.all()
         article_id = request.GET.get('id')
         article = Article.objects.get(pk=article_id)
-        return render(request, 'front/article.html', {'article': article})
+        return render(request, 'front/article.html', {'categories': categories, 'article': article})
+
+
+def search_articles(request):
+    categories = ArticleCategory.objects.all()
+    articles = Article.objects.filter(publish=True)
+    search_string = ''
+
+    if request.method == 'GET':
+
+        if 'title' in request.GET:
+            search_string += '&title=' . request.GET.get('title')
+            articles = articles.filter(title__contains=request.GET.get('title'))
+
+        if 'artist_name' in request.GET:
+            search_string += '&artist_name=' . request.GET.get('artist_name')
+            articles = articles.filter(work_link__creator__name__contains=request.GET.get('artist_name'))
+
+        if 'tags' in request.GET:
+            search_string += '&tags=' . request.GET.get('tags')
+            articles = articles.filter(tags__contains=request.GET.get('tags'))
+
+    articles = articles.order_by('-date_created')
+
+    pager = Paginator(articles, articles_per_page)
+
+    if request.method == 'GET' and 'page' in request.GET:
+        page = request.GET.get('page')
+    else:
+        page = 1
+
+    try:
+        articles = pager.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        articles = pager.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        articles = pager.page(pager.num_pages)
+    return render(request, 'front/search_articles.html', {'categories': categories, 'articles': articles, 'total_results': pager.count, 'number_of_pages': pager.num_pages, 'page': page, 'search_string': search_string})
