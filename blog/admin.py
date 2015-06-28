aee899b76d81ae14d614c5fc3f5562b86a8945b4
pattmayne <pattmayne@gmail.com>
2015-06-28 00:45:04 -0400

    Fixin the front page

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from blog.models import Article, Tag, Author, ArticleCategory
+from blog.models import Article, Tag, Author, ArticleCategory, RelativeEpoch
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Author)
 admin.site.register(Tag)
-admin.site.register(Tag)
-#admin.site.register(RelativeEpoch)
+admin.site.register(RelativeEpoch)
 admin.site.register(ArticleCategory)
 #admin.site.register(ArticleImage)
