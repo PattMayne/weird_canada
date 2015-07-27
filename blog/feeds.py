441c4175f93d62c9708ba112d58e59e98cb4d991
pattmayne <pattmayne@gmail.com>
2015-07-26 20:10:18 -0400

    Edit Tracklist Stuff

--- blog/feeds.py
+++ blog/feeds.py
@@ -20,4 +20,4 @@ class LatestArticlesFeed(Feed):
     # item_link is only needed if NewsItem has no get_absolute_url method.
     def item_link(self, item):
         #return reverse('news-item', args=[item.pk])
-        return "text"
+        return "/article/?id=" + item.id
